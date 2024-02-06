0d687b2ed9e02c0b88f6eba9bdb946326b0171b6
the7ag <mohamed.dead1543@gmail.com>
2024-02-06 14:30:58 +0200

    Added motor driver not yet tested

---
+++ HAL/MotorDriver/MD_config.h
@@ -0,0 +1,19 @@
+/**
+ * @file MD_config.h
+ * @brief This file contains the public interface for the US HAL drivers.
+ *
+ * @copyright Copyright (c) 2024
+ * 
+ * This driver library streamlines the integration and utilization of ultrasonic sensors,
+ *  offering precise distance measurement capabilities in embedded systems with efficient handling of sensor data and configuration.
+ *
+ * @note This module is designed to be used with ARM Cortex-M processors, and may not be compatible with other architectures.
+ *
+ * @date 06 feb 2024
+ * @version V01
+ * @author Mohamed Ali Bayoumi
+ */
+#ifndef MD_CONFIG_H_
+#define MD_CONFIG_H_
+
+#endif /* MD_CONFIG_H_ */
